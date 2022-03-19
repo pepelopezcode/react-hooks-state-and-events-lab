def8c7ab4c876d8ba2b1b43d94c13d6d194424ee
pepe lopez <plopez741@gmail.com>
2022-03-19 10:13:49 -0700

    commit tree 41d82abdefee2e10b2e25b9f8c7fbad3d5c0990d

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const[selectedCategory, setCategory] = useState("All")
+
+ const newItems = items.filter((item) => {
+   if(selectedCategory === "All"){
+     return item
+   }else{return item.category === selectedCategory}
+   
+ })
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange={(event) => setCategory(event.target.value)}>
+          <option value="All" >Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {newItems.map((item) => (<Item key={item.id} name={item.name} category={item.category} />))}
       </ul>
     </div>
   );
 }
 
 export default ShoppingList;
+// items.map((filterItems(items)) => (
+          
+//         ))
